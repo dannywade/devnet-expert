8b0bd578dd95ba35b433a28b84715ad57c20ae5f
dannywade <danny.wade35@gmail.com>
2022-03-05 15:37:33 -0500

    reformatted with black

--- webhook/meraki_receiver.py
+++ webhook/meraki_receiver.py
@@ -25,6 +25,7 @@ def validator():
     """
     return MERAKI_VALIDATOR_STRING
 
+
 # Route for Meraki event data
 @app.route("/", methods=["POST"])
 def data_intake():
@@ -35,7 +36,7 @@ def data_intake():
     - Parse interesting data to be sent to user
     - Send interesting data to Webex Teams
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         # Debugging purposes
         print("Data received from Webhook is: ", request.json)
 
@@ -52,20 +53,20 @@ def data_intake():
 
         # Base URL to post messages to Webex
         webex_url = "https://webexapis.com/v1/messages"
-        
+
         # Payload for Webex Messages API
-        message_body = json.dumps({
-            "roomId": WEBEX_ROOM_ID,
-            "markdown": webex_message
-        })
+        message_body = json.dumps({"roomId": WEBEX_ROOM_ID, "markdown": webex_message})
 
         # Required headers for Webex Messages API call
-        webex_headers = { 
+        webex_headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {WEBEX_BOT_TOKEN}"}
+            "Authorization": f"Bearer {WEBEX_BOT_TOKEN}",
+        }
 
         # Send parsed data to Webex Teams room
-        webex_post = requests.post(url=webex_url, headers=webex_headers, data=message_body, verify=False)
+        webex_post = requests.post(
+            url=webex_url, headers=webex_headers, data=message_body, verify=False
+        )
 
         # Print out response body and status code
         print(webex_post.status_code)
@@ -78,6 +79,7 @@ def data_intake():
             print("Error sending to Webex")
 
         return "Webhook received!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
