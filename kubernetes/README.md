8b5ffd001d8a2d50bb4fabbc7ee7ad0b0d5188fa
dannywade <danny.wade35@gmail.com>
2022-03-27 17:06:28 -0400

    added real-world examples

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -1,3 +1,6 @@
 ## Examples
 #### DevNet App Example - K8 manifest that deploys a namespace, service, and deployment (custom Flask app)
 #### Webserver Pod - K8 manifest that deploys a single pod (nginx container)
+
+## Real-World Examples
+### Look in `app-examples` directory. All examples were based on examples from Kubernetes Up & Running book (2nd Edition) - O'Reilly
---
+++ kubernetes/app-examples/ghost-blog/ghost-config.js
@@ -0,0 +1,24 @@
+// Simple configuration for Ghost
+var path = require('path'), config;
+
+config = {
+    development: {
+        url: 'http://localhost:2368',
+        database: {
+            client: 'mysql',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    }
+};
+
+module.exports = config;
